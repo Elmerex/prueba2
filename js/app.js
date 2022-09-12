53d49b01339416e8c3db5a72bd9886915beb78cf
Elmerex <humbersber@gmail.com>
2022-09-12 19:47:26 -0300

    clase 5

--- js/app.js
+++ js/app.js
@@ -1,4 +1,74 @@
-let var1 = 0;
-var var2 = 1;
+/*--------------------------Clase03--------------------------------
+const myDoc = document;
 
-const myDom = document;
+console.log(myDoc);
+
+let myH1 = myDoc.getElementsByTagName("h1");
+
+let myH3 = myDoc.getElementById("h3id");
+
+console.log(myH1);
+console.log(myH3);
+
+myH3.innerText = "Ahora cambio";
+myH3.innerHTML = "<button>Esto es un boton</button>";
+
+const myButtonStr = "<button>Boton Azul</button>";
+
+//while, do while, for
+
+var cant_elementos = parseInt(prompt("cuantos botones?"));
+
+for (let index = 0; index < cant_elementos; index++) {
+    myH3.innerHTML = myH3.innerHTML + myButtonStr;
+}
+*/
+
+
+
+
+/*--------------------------Clase04--------------------------------
+
+let myDom = document;
+
+const myDog = {
+    "name": "Macho y pure",
+    "age": 3,
+    "legs": 4
+};
+
+const myCat = {
+    name: "Nikita",
+    age: 15,
+    legs: 4
+};
+
+console.log(myDog.name);
+console.log(myCat.name);
+
+myCat.tail =1;
+
+console.log(myCat.tail);
+
+
+//Creado de clase y constructor
+class Mouse {
+    constructor(name, legs, tail) {
+        this.name = name;
+        this.legs = legs;
+        this.tail = tail;
+    }
+}
+
+//Nuevo objeto asignando valores
+const myMouse = new Mouse("Perez",4,1);
+
+console.log(myMouse);*/
+
+/*--------------------------Clase05--------------------------------
+
+class*/
+
+/*--------------------------Clase05--------------------------------*/
+
+
